df8ad4e3ffa50f2a9b830020b5b6e72e0e10848b
andriideikalo <wlada801@gmail.com>
2023-01-26 11:44:16 +0200

    fix

---
+++ src/components/API/API.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_KEY = '31949883-7c5fe764cd95888a750d50db1';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const getImages = (query, page) => {
+  return axios
+    .get(
+      `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12
+`
+    )
+    .then(res =>
+      res.data.hits.map(({ id, webformatURL, largeImageURL }) => ({
+        id,
+        webformatURL,
+        largeImageURL,
+      }))
+    );
+};
+
+export default { getImages };

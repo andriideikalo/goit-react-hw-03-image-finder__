49fbfe27172ec7b9e5c06804f49f26271f4356e8
andriideikalo <wlada801@gmail.com>
2023-01-26 12:50:41 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,88 @@
-// import { Component } from 'react';
+import React, { Component } from 'react';
 // import api from './API/API';
-// import Searchbar from './Searchbar/Searchbar';
-// import ImageGallery from './ImageGallery/ImageGallery';
-// import Loader from './Loader/Loader';
-// import Button from './Button/Button';
-// import Modal from './Modal/Modal';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
 
-// class App extends Component {
-//   state = {
-//     images: [],
-//     modalImage: null,
-//     query: '',
-//     page: 1,
-//     showModal: false,
-//     isLoading: false,
-//   };
+export class App extends Component {
+  state = {
+    images: [],
+    modalImage: null,
+    query: '',
+    page: 1,
+    showModal: false,
+    isLoading: false,
+  };
 
-// componentDidUpdate(prevProps, prevState) {
-//   if (
-//     prevState.query !== this.state.query ||
-//     prevState.page !== this.state.page
-//   ) {
-//     api
-//       .getImages(this.state.query, this.state.page)
-//       .then(images =>
-//         this.setState(prev => ({ images: [...prev.images, ...images] }))
-//       )
-//       .catch(console.log)
-//       .finally(() => this.setState({ isLoading: false }));
-//   }
-// }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (
+  //     prevState.query !== this.state.query ||
+  //     prevState.page !== this.state.page
+  //   ) {
+  //     api
+  //       .getImages(this.state.query, this.state.page)
+  //       .then(images =>
+  //         this.setState(prev => ({ images: [...prev.images, ...images] }))
+  //       )
+  //       .catch(console.log)
+  //       .finally(() => this.setState({ isLoading: false }));
+  //   }
+  // }
 
-// export const App = () => {
-// return (
-//   <div
-//     style={{
-//       height: '100vh',
-//       display: 'flex',
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//       fontSize: 40,
-//       color: '#010101',
-//     }}
-//   >
-//     React template
-//   </div>
-// );
-// }
+  onSearchSubmit = query => {
+    if (query === this.state.query) return;
+
+    this.setState({
+      query: query,
+      page: 1,
+      images: [],
+      showModal: false,
+      isLoading: true,
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState(prev => ({ page: prev.page + 1, isLoading: true }));
+  };
+
+  onImageClick = largeImage =>
+    this.setState({ showModal: true, modalImage: largeImage });
+
+  onCloseModal = () => this.setState({ showModal: false });
+
+  render = () => {
+    const { isLoading, showModal, images, modalImage } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.onSearchSubmit} />
+        {images.length > 0 && (
+          <ImageGallery images={images} onImageClick={this.onImageClick} />
+        )}
+        {showModal && (
+          <Modal image={modalImage} onCloseModal={this.onCloseModal} />
+        )}
+        {isLoading && <Loader />}
+        {images.length > 0 && !isLoading && (
+          <Button onClick={this.onLoadMore} />
+        )}
+      </>
+    );
+  };
+
+  // <div
+  //   style={{
+  //     height: '100vh',
+  //     display: 'flex',
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //     fontSize: 40,
+  //     color: '#010101',
+  //   }}
+  // >
+  //   React template
+  // </div>
+}
+
+export default App;

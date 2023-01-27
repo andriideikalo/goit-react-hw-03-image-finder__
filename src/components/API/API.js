70258a7f33a4c4b107cec1d246ec480adb4d0013
andriideikalo <wlada801@gmail.com>
2023-01-27 16:02:31 +0200

    fix

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -1,20 +1,21 @@
-// // import axios from 'axios';
+import axios from 'axios';
 
-// const API_KEY = '31949883-7c5fe764cd95888a750d50db1';
-// const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '31949883-7c5fe764cd95888a750d50db1';
+const BASE_URL = 'https://pixabay.com/api/';
 
-// export const getImages = (query, page) => {
-//   return get(
-//     `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12
-// `
-//   );
-//   // .then(res =>
-//   //   res.data.hits.map(({ id, webformatURL, largeImageURL }) => ({
-//   //     id,
-//   //     webformatURL,
-//   //     largeImageURL,
-//   //   }))
-//   // );
-// };
+export const getImages = (query, page) => {
+  return axios
+    .get(
+      `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12
+`
+    )
+    .then(res =>
+      res.data.hits.map(({ id, webformatURL, largeImageURL }) => ({
+        id,
+        webformatURL,
+        largeImageURL,
+      }))
+    );
+};
 
-// export default { getImages };
+export default { getImages };

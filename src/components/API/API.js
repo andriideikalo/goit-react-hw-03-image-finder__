f4be4de70b2df71db2e268fe0b8359f0e6ce8b21
andriideikalo <wlada801@gmail.com>
2023-01-27 15:41:09 +0200

    Update API.js

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const API_KEY = '31949883-7c5fe764cd95888a750d50db1';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export const getImages = (query, page) => {
-  return axios.get(
+  return get(
     `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12
 `
   );
